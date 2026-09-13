3ebf6374f7fb3b7d43f8507882d1c5f81a53a7d1
unitycubed <ebiglari@gmail.com>
2026-09-12 22:15:11 -0500

    package.json: fix repository URL to unitycubed/tldrapi-nodejs

--- dist/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/** Public result + option types for the TLDRapi SDK. */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VALID_TIERS = void 0;
-exports.VALID_TIERS = ['quick', 'standard', 'deep', 'premium', 'ultra'];
-//# sourceMappingURL=types.js.map
